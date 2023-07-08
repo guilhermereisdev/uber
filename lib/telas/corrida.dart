3b547c732a9258ad36ee489e71ebadd31f62e8a9
Guilherme Reis <guilhermereis2009@hotmail.com>
2023-07-08 04:59:40 -0300

    Exibe os marcadores do motorista e do passageiro no mapa após o motorista aceitar a viagem

--- lib/telas/corrida.dart
+++ lib/telas/corrida.dart
@@ -23,7 +23,7 @@ class _CorridaState extends State<Corrida> {
   CameraPosition _cameraPosition = const CameraPosition(
     target: LatLng(-23.563999, -46.653256),
   );
-  final Set<Marker> _marcadores = {};
+  Set<Marker> _marcadores = {};
   Map<String, dynamic>? _dadosRequisicao;
   Position? _localMotorista;
   String _textoBotao = "Aceitar corrida";
@@ -146,6 +146,58 @@ class _CorridaState extends State<Corrida> {
 
   _statusACaminho() {
     _alteraBotaoPrincipal("A caminho do passageiro", Colors.grey, () {});
+    double latitudePassageiro = _dadosRequisicao?["passageiro"]["latitude"];
+    double longitudePassageiro = _dadosRequisicao?["passageiro"]["longitude"];
+    double latitudeMotorista = _dadosRequisicao?["motorista"]["latitude"];
+    double longitudeMotorista = _dadosRequisicao?["motorista"]["longitude"];
+    _exibirDoisMarcadores(
+      LatLng(latitudeMotorista, longitudeMotorista),
+      LatLng(latitudePassageiro, longitudePassageiro),
+    );
+  }
+
+  _exibirDoisMarcadores(LatLng latLngMotorista, LatLng latLngPassageiro) {
+    double pixelRatio = MediaQuery.of(context).devicePixelRatio;
+    Set<Marker> listaMarcadores = {};
+
+    BitmapDescriptor.fromAssetImage(
+      ImageConfiguration(devicePixelRatio: pixelRatio),
+      "images/motorista.png",
+    ).then((icone) {
+      Marker marcadorMotorista = Marker(
+        markerId: const MarkerId("marcador-motorista"),
+        position: LatLng(latLngMotorista.latitude, latLngMotorista.longitude),
+        infoWindow: const InfoWindow(title: "Local motorista"),
+        icon: icone,
+      );
+      listaMarcadores.add(marcadorMotorista);
+    });
+
+    BitmapDescriptor.fromAssetImage(
+      ImageConfiguration(devicePixelRatio: pixelRatio),
+      "images/passageiro.png",
+    ).then((icone) {
+      Marker marcadorPassageiro = Marker(
+        markerId: const MarkerId("marcador-passageiro"),
+        position: LatLng(latLngPassageiro.latitude, latLngPassageiro.longitude),
+        infoWindow: const InfoWindow(title: "Local passageiro"),
+        icon: icone,
+      );
+      listaMarcadores.add(marcadorPassageiro);
+    });
+
+    setState(() {
+      _marcadores = listaMarcadores;
+      _movimentarCamera(
+        CameraPosition(
+          target: LatLng(
+            latLngMotorista.latitude,
+            latLngMotorista.longitude,
+          ),
+          zoom: 18,
+        ),
+      );
+    });
   }
 
   _aceitarCorrida() async {
